7dad11ad3eb8b0796a52e0f1edd9523eb7f3fb45
hheeju98 <hheeju98@naver.com>
2023-09-01 12:39:48 +0900

    3차 코드리뷰

--- 계산기/index.js
+++ 계산기/index.js
@@ -1,24 +1,43 @@
 let currentNumber = "";
 //let currentNumber2 = "";
 let currentOperator = "";
-const inputField = document.querySelector(".inputField");
-const outputField = document.querySelector(".outputField");
+let outputField2 = ""; // 연산자 저장
+let one = ""; // 첫번쨰 값 고정
+let two = "";
+let result = "";
+// 변수 여러개 등록되는 거 수정
+// 첫번째 값으로 무조건 숫자만 등록되도록 수정
 
 window.onload = () => {
   const tdElements = document.querySelectorAll("td");
   //console.log(tdElements); 유사배열
   const tdElementsArray = Array.from(tdElements);
 
+  //파일분리 콘솔주석 지유기
   tdElementsArray.map((td) => {
     td.addEventListener("click", simple);
   });
+
   //console.log(tdElementsArray);
+  calculation = (clickedValue) => {
+    const outputField = document.querySelector(".outputField");
+    console.log(outputField.value);
+    try {
+      let display = document.querySelector(".outputField");
+      display.value = eval(display.value);
+      console.log(display.value);
+    } catch {
+      alert("올바른 계산식을 입력해 주세요");
+    }
+  };
 };
 
 const simple = (e) => {
+  const inputField = document.querySelector(".inputField");
+  const outputField = document.querySelector(".outputField");
   const clickedValue = e.target.innerText;
   if (!isNaN(clickedValue)) {
-    currentNumber += clickedValue;
+    one += clickedValue;
     const formattedNumber = formatNumber(currentNumber);
     outputField.value = formattedNumber;
   } else if (isOperator2(clickedValue) === true) {
@@ -34,6 +53,7 @@ const simple = (e) => {
     calculation();
   }
 };
+
 const formatNumber = (number) => {
   return number.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 };
@@ -45,7 +65,7 @@ const isOperator = (value) => {
   } else {
     return false;
   }
-  //return ["+", "-", "×", "÷"].includes(value);
+  //return ["+", "-", "×", "÷"].includes(value); include다시 쓰기
 };
 
 const isOperator2 = (value) => {
@@ -56,9 +76,9 @@ const isOperator2 = (value) => {
   } else {
     return false;
   }
-  //return ["+", "-", "×", "÷"].includes(value);
+  //return ["+", "-", "×", "÷"].includes(value); 사용
 };
-
+// reduce 써보기
 //const calculation = (num1, operator, num2) => {
 // if (operator === "+") {
 //   return num1 + num2;
@@ -70,13 +90,3 @@ const isOperator2 = (value) => {
 //  return num1 / num2;
 // }
 //};
-function calculation(clickedValue) {
-  try {
-    outputField.value = currentNumber += clickedValue;
-    let display = document.querySelector(".outputField");
-    display.value = eval(display.value);
-    console.log(display.value);
-  } catch {
-    alert("올바른 계산식을 입력해 주세요");
-  }
-}
