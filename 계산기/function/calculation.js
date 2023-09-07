f1f53218818165c5bd939c23fb68bd02d6855237
hheeju98 <hheeju98@naver.com>
2023-09-07 18:31:51 +0900

    reduce 주석

--- 계산기/function/calculation.js
+++ 계산기/function/calculation.js
@@ -29,3 +29,4 @@ export function calculation1(num1, operator, num2) {
     }
   });
 }
+//주어진 코드에서 calculation1 함수가 값을 반환하지 않고 arr.reduce 메서드 내에서 연산을 수행한 후에 결과를 반환하지 않기 때문에 계산 결과가 제대로 반환되지 않습니다. arr.reduce 메서드는 누적 값을 반환해야 합니다.
