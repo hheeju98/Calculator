ad4e3056657ec6030b50a1cede3fad2b0a90606e
hheeju98 <hheeju98@naver.com>
2023-09-18 01:28:39 +0900

    삭제 수정중

--- 계산기/function/click.js
+++ 계산기/function/click.js
@@ -30,35 +30,8 @@ export function click(click) {
   }
   if (!isNaN(click)) {
     numberArray.push(totalData);
-  }
-
-  if (isOperator(click) == true && one !== "" && operatorClicked == true) {
-    currentOperator = click;
-    outputField.value += click;
-    operatorClicked = false;
-  } else if (
-    !isNaN(click) &&
-    currentOperator === "" &&
-    inputField.value == ""
-  ) {
-    // 제일 첫번째 값이 숫자일때 one이다
-    one += click;
-    outputField.value += click;
-    operatorClicked = true;
-    // 3+4*5+3
-  } else if (!isNaN(click) && currentOperator !== "" && one !== "") {
-    // 두번째 숫자가 있을 경우 그 값은 two
-    // 결과 계산 -> 결과를 one에 넣기
-    two += click;
-    result = calculation(one, currentOperator, two);
-    two = "";
-    one = result;
-    operatorClicked = true;
-    outputField.value += click;
-
-    if (inputField.value !== "" && isOperator(click)) {
-      outputField.value += click;
-    }
+    console.log(totalArray[0]);
+    console.log(totalArray.values);
   } else if (click === "c") {
     currentOperator = "";
     one = "";
@@ -68,7 +41,39 @@ export function click(click) {
     operatorClicked = true;
     totalArray.length = 0;
     calculateArray.length = 0;
-  } else if (click === "=" && !isNaN(totalArray[totalArray.length - 2].value)) {
+  } else if (click === "=") {
+    console.log(totalArray[0]);
+    if (
+      !isNaN(
+        totalArray[0].value && currentOperator === "" && inputField.value == ""
+      )
+    ) {
+      one = totalArray[0].value;
+      console.log(one);
+      console.log(totalArray[1].value);
+      console.log(totalArray[1].type);
+    }
+
+    currentOperator = totalArray[1].value;
+    console.log(currentOperator);
+    //   outputField.value += totalArray.values;
+    operatorClicked = false;
+
+    if (!isNaN(totalArray[2].value) && currentOperator !== "" && one !== "") {
+      two = totalArray[2].value;
+      console.log(two);
+      result = calculation(one, currentOperator, two);
+      console.log(result);
+      two = "";
+      one = result;
+      operatorClicked = true;
+      //      outputField.value = totalArray.value;
+
+      if (inputField.value !== "" && isOperator(click)) {
+        outputField.value += click;
+      }
+    }
+
     inputField.value = result;
     outputField.value = "";
     operatorClicked = true;
@@ -120,3 +125,28 @@ export function click(click) {
     }
   }
 }
+
+// if (isOperator(click) == true && one !== "" && operatorClicked == true) {
+//   currentOperator = click;
+//   outputField.value += click;
+//   operatorClicked = false;
+// } else if (!isNaN(click) && currentOperator === "" && inputField.value == "") {
+//   // 제일 첫번째 값이 숫자일때 one이다
+//   one += click;
+//   outputField.value += click;
+//   operatorClicked = true;
+//   // 3+4*5+3
+// } else if (!isNaN(click) && currentOperator !== "" && one !== "") {
+//   // 두번째 숫자가 있을 경우 그 값은 two
+//   // 결과 계산 -> 결과를 one에 넣기
+//   two += click;
+//   result = calculation(one, currentOperator, two);
+//   two = "";
+//   one = result;
+//   operatorClicked = true;
+//   outputField.value += click;
+
+//   if (inputField.value !== "" && isOperator(click)) {
+//     outputField.value += click;
+//   }
+// }
